1ed1d0d1f120362d0fc682eff5a09f64b4aa7462
Thomas Phung <81277197+thomasphung@users.noreply.github.com>
2026-09-23 11:55:01 -0400

    Update site_url to use 'www' subdomain (canonical)

--- postprocess.py
+++ postprocess.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     postprocess_site(
         site_dir="html",  # Your build output directory
         docs_dir="markdown",  # Your source docs directory
-        site_url="https://fedramp.gov/join",
+        site_url="https://www.fedramp.gov/join",
         default_image="https://www.fedramp.gov/join/assets/frcs.jpg",
         default_author="pete@fedramp.gov",
         add_desc=True,
